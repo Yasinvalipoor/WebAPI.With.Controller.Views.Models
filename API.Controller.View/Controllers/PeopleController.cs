47826e5555dbee10a6041b9ee217e5217fbc58ce
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-05-03 10:06:40 +0330

    Add project files.

---
+++ API.Controller.View/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using API.Controller.View.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controller.View.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class PeopleController : ControllerBase
+    {
+        private readonly WebApiControllerViewContext dbcontext;
+
+        public PeopleController(WebApiControllerViewContext Dbcontext)
+        {
+            dbcontext = Dbcontext;
+        }
+
+        [HttpGet("GetAllPeople")]
+        public IActionResult GetAllPeople()
+        {
+            return Ok(dbcontext.people.ToList());
+        }
+    }
+}
---
+++ API.Controller.View/Models/WebApiControllerViewContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controller.View.Models;
+
+public partial class WebApiControllerViewContext : DbContext
+{
+    public DbSet<Person> people { get; set; }
+
+
+    public WebApiControllerViewContext()
+    {
+    }
+
+    public WebApiControllerViewContext(DbContextOptions<WebApiControllerViewContext> options)
+        : base(options)
+    {
+    }
+}
